69e1ae67d8a3ba9182d130b52585904539d94278
cannondork <eirikpro@gmail.com>
2017-04-04 15:52:14 +0200

    Final commit

--- jsonurl/main.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "./jsonurlv2"
-
-func main() {
-	//readurl.ExDecodeMine("http://samples.openweathermap.org/data/2.5/weather?zip=94040%2Cus&appid=b1b15e88fa797225412429c1c50c122a1")
-	jsonurl.JsonUrl()
-}
--- readurl/main.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "./readurl"
-
-func main() {
-	readurl.ExDecodeMine()
-
-}
